755e09eda8b3942c92d303f630da3ca2cfbe521b
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2019-07-22 22:26:59 -0300

    criado_dispositivosmercos_enums
    Criei o arquivo enums dos dispositivos, criando assim algumas tupulas para forms e pesquisas furutas

---
+++ dispositivosmercos/enums.py
@@ -0,0 +1,14 @@
+# sistema operacional dos dispositivos
+
+DISPOSITIVOS_OS = (
+    ('1', 'Android'),
+    ('2', 'iOS')
+)
+
+# dispositivos disponiveis para emprestino ios
+
+DISPOSITIVOS_IOS = ()
+
+# dispositivos disponiveis para emprestino android
+
+DISPOSITIVOS_AND = ()
