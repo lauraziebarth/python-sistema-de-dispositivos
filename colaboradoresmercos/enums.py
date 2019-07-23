d12743f6e76ff39cce3d972c70c0da6772e15fe9
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2019-07-22 22:27:33 -0300

    atualizado_colaboradoresmercos_enums
    Adicionei o restante das areas em AREAS

--- colaboradoresmercos/enums.py
+++ colaboradoresmercos/enums.py
@@ -1,6 +1,9 @@
+# area da empresa que o colaborador escolhe ao fazer o cadastro
+
 AREAS = (
 
     ('cs', 'Customer Success'),
     ('dev', 'Desenvolvimento'),
+    ('canais', 'Canais'),
+    ('vendas', 'Inside Sales')
 )
-
