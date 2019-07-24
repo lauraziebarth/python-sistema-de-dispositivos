1626f79da1481c26567de50be05ef0ac23f91bef
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2019-07-24 18:41:18 -0300

    def_salvar_imagem_init
    Criei uma função para salvar a imagem, só para iniciar.

--- core/colaborador/__init__.py
+++ core/colaborador/__init__.py
@@ -8,3 +8,6 @@ def cria_novo_colaborador(user_id, nome, email, area):
     colaborador.email = email
     colaborador.area = area
     colaborador.save()
+
+def salva_imagem_colaborador():
+    pass
