91f97b7b3b90b90e2f77bc5a024c5dd1accaa53b
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2019-08-01 11:41:20 -0300

    novas views em dispositivosmercos
    Criada a view de emprestar e devolver dispositivos.
    
    Acrescentei tbm a necessidade de login para visualização das views.

--- dispositivosmercos/views.py
+++ dispositivosmercos/views.py
@@ -7,12 +7,16 @@ from django.urls import reverse
 from django.contrib.auth import authenticate
 from dispositivosmercos.gateway import busca_dispositivos_nao_excluidos, busca_um_dispositivo, alterar_dispositivo, excluir_dispositivo, cria_novo_dispositivo
 
+
+@login_required
 class ListarDispositivos(View):
     def get(self, request):
         dispositivos = busca_dispositivos_nao_excluidos()
         return render(request, 'listar_dispositivos.html', {'dispositivos': dispositivos})
 
 
+@login_required
+@administrador_required
 class CadastrarDispositivo(View):
     def get(self, request):
         form = FormDispositivo()
@@ -35,6 +39,8 @@ class CadastrarDispositivo(View):
         return redirect(reverse('listar_dispositivos'))
 
 
+@login_required
+@administrador_required
 class AlterarDispositivo(View):
     def get(self, request, dispositivo_id=None):
         dispositivo = busca_um_dispositivo(dispositivo_id)
@@ -58,8 +64,44 @@ class AlterarDispositivo(View):
         return redirect(reverse('listar_dispositivos'))
 
 
+@login_required
+@administrador_required
 class ExcluirDispositivo(View):
-    def get(self, request, dispositivo_id=None):
+    def get(self, dispositivo_id=None):
         excluir_dispositivo(dispositivo_id)
         return redirect(reverse('listar_dispositivos'))
 
+
+@login_required
+class EmprestarDispositivo(View):
+    def get(self, request, dispositivo_id=None):
+        dispositivo = busca_um_dispositivo(dispositivo_id)
+        form = FormDispositivo(initial={'id': dispositivo.id, 'nome': dispositivo.nome, 'descricao': dispositivo.descricao, 'numeromodelo': dispositivo.numeromodelo, 'versao': dispositivo.versao})
+        return render(request, 'emprestar_dispositivo.html', {'form': form, 'dispositivo_id': dispositivo_id})
+
+    def post(self, request, dispositivo_id=None):
+        form = FormDispositivo(request.POST)
+
+        if not form.is_valid():
+            return render(request, 'alterar_dispositivo.html', {'form': form, 'dispositivo_id': dispositivo_id})
+
+        pass
+
+       # AQUI TEMOS QUE COLOCAR O VINCULO ENTRE DEVICE E COLABORADOR
+
+
+@login_required
+class DevolverDispositivo(View):
+    def get(self, request, dispositivo_id=None):
+        dispositivo = busca_um_dispositivo(dispositivo_id)
+        form = FormDispositivo(initial={'id': dispositivo.id, 'nome': dispositivo.nome, 'descricao': dispositivo.descricao, 'numeromodelo': dispositivo.numeromodelo, 'versao': dispositivo.versao})
+        return render(request, 'emprestar_dispositivo.html', {'form': form, 'dispositivo_id': dispositivo_id})
+
+    def post(self, request, dispositivo_id=None):
+        form = FormDispositivo(request.POST)
+
+        if not form.is_valid():
+            return render(request, 'alterar_dispositivo.html', {'form': form, 'dispositivo_id': dispositivo_id})
+
+        pass
+       # AQUI TEMOS QUE COLOCAR O VINCULO ENTRE DEVICE E COLABORADOR
