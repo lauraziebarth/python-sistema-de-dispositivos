a4c6a4528a30bc5e69a63b2c5234d67c10e8c517
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2020-02-14 13:04:02 -0200

    ajuste no banco

--- projeto_20/settings.py
+++ projeto_20/settings.py
@@ -16,9 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i3lswg)z#xxaidb(n^$nn5x=y-ac2n()gwba_2)nkf5+(4afu@'
 
@@ -78,18 +75,13 @@ WSGI_APPLICATION = 'projeto_20.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "PostgreSQL 12",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
         "USER": "lauragabrielaziebarth",
         "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "5432",
-    },
-
-    'local': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
-        }
+    }
 }
 
 
