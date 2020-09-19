d4b5e3053313fa64c1b2017b9366c02262afd5b6
Laura Ziebarth <38667871+lauraziebarth@users.noreply.github.com>
2020-09-19 15:13:32 -0300

    Adicionado teste para função busca_um_dispositivo

---
+++ dispositivos/tests/test_busca_um_dispositivo.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from dispositivos.gateway import busca_um_dispositivo
+from dispositivos.tests.core_test_case import criar_dispositivo_android, criar_dispositivo_ios
+
+
+class BuscaUmDispositivo(TestCase):
+    def setUp(self):
+        self.dispositivo_android = criar_dispositivo_android()
+        self.dispositivo_ios = criar_dispositivo_ios()
+
+    def test_retorna_um_unico_dispositivo(self):
+        resultado = (busca_um_dispositivo(self.dispositivo_android.id))
+
+        self.assertEqual(1, resultado.id)
